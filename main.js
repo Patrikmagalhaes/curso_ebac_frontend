31f36f500e98b259bb7a47f9924ba6d621d3556b
Patrikmagalhaes <patrikmagalhaes@icloud.com>
2023-11-06 17:46:01 -0300

    adicionando imagem

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
 $(document).ready(function () {
-    $("header button").click(function(){
+    $("header button").click(function () {
         $("form").slideDown()
     })
-    $("#cancelar").click(function(){
+    $("#cancelar").click(function () {
         $("form").slideUp()
     })
 
-    $("form").on("submit", function (e) { 
+    $("form").on("submit", function (e) {
         e.preventDefault()
-        const imagemNova = $("#urlImagemNova").val()
-        const imagem = $["<li></li>"]
+        const urlImagem = $("#urlImagemNova").val()
+        console.log(`endereço: ${urlImagem}`)
+        const novoItem = $("<li></li>")
+        $(`<img src="${urlImagem}"/>`).appendTo(novoItem)
+
+        $(`<div class="orverlay-image-link">
+            <a href="${urlImagem}" target="_blank" title="Ver imagem em tamanho real">
+          Ver imagem em tamanho real
+            </a>
+            </div>`).appendTo(novoItem)
+            $(novoItem).appendTo("ul")
     })
 
 })
