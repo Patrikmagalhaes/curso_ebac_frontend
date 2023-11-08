593d26e69e75a796e25fa966429ca5fd427a3f53
Patrikmagalhaes <patrikmagalhaes@icloud.com>
2023-11-08 16:02:54 -0300

    ADD Funcionalidade deixar riscado o item da lista

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 $(document).ready(function () {
+    let tarefasSalvas = []
     $('form').on('submit', function (e) {
         e.preventDefault()
         const tarefa = $('input').val()//pega o valor do input (nome da tarefa)
-        const listaDeTarefa = $(`#lista`)//seleciona a tag ol onde sera adicionada as li's
-        $(`<li>${tarefa}</li>`).appendTo(listaDeTarefa)//adiciona a tag li dentro da tag ol com o valor do input (nome da tarefa) 
-
-
-        
-
+        tarefa.trim()//limpa espaços antes e depois 
+        if (tarefasSalvas.includes(tarefa.value) || tarefa == null) {
+            alert("A tarefa ja existe !")
+            $('input').val("")
+        } else {
+            tarefasSalvas.push(tarefa.value)
+            const listaDeTarefa = $(`#lista`)//seleciona a tag ol onde sera adicionada as li's
+            $(`<li>${tarefa}</li>`).appendTo(listaDeTarefa)//adiciona a tag li dentro da tag ol com o valor do input (nome da tarefa) 
+        }
+        $(`#lista`).click(function () {
+            $(this).css("text-decoration", "line-through")
+        })
     })
 })
