52851b1e407efc2cd41fa40c83a9694ebcd6422c
Patrikmagalhaes <patrikmagalhaes@icloud.com>
2023-11-09 13:55:43 -0300

    add validacaode inputs

--- main.js
+++ main.js
@@ -2,30 +2,33 @@ $(document).ready(function () {
     let tarefasSalvas = []
     $('form').on('submit', function (e) {
         e.preventDefault()
-        const tarefa = $('input').val()//pega o valor do input (nome da tarefa)
-        tarefa.trim()//limpa espaços antes e depois 
-        if (tarefasSalvas.includes(tarefa.value)) {
+        const tarefa = $('input').val().trim("")/*pega o valor do input (nome da tarefa)
+        e limpa espaços antes e depois das palavras*/
+
+        //verifica se existe uma tarefa igual
+        if (tarefasSalvas.includes(tarefa)) {
             alert("A tarefa ja existe !")
             $('input').val("")
-        } else {
-            tarefasSalvas.push(tarefa.value)
+        } else if (tarefa === "") { alert("Digite uma tarefa válida!") }
+        else {
+            tarefasSalvas.push(tarefa)
             const listaDeTarefa = $(`#lista`)//seleciona a tag ul onde sera adicionada as li's
             $(`<li>${tarefa}</li>`).appendTo(listaDeTarefa)//adiciona a tag li dentro da tag ul com o valor do input (nome da tarefa) 
         }
 
         let on = [1]
-        $(`#lista`).click(function () {
-          
-            if (on[0] === 1 ) {
+        $(`li`).click(function () {
+            //se for verdadeiro risca a atividade e atualiza o valor pra 0
+            if (on[0] === 1) {
                 $(this).addClass("tarefa-feita")
                 on = []
-                on.push(0) 
-            } else {
+                on.push(0)
+            } else {//remove o risco da atividade e atualiza o valor pra 1
                 $(this).removeClass("tarefa-feita")
                 $(this).addClass(".na-feita")
                 on = []
                 on.push(1)
-            
+
             }
         })
     })
