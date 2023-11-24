31ae631ed7d9eeae21b942b4bbf4bbf52a7efd08
Patrikmagahaes <patrikmagalhaes@icloud.com>
2023-11-24 20:55:01 -0300

    add evento ao formulario

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 
 document.querySelector('form').addEventListener("submit", function (e) {
-    e.preventDefault()
+   
 
     document.getElementById('alerta').classList.remove("off")
-    document.getElementById('nome').innerHTML = ""
+    document.getElementById('nome').value = ""
+    document.getElementById('tel').value = ""
+    document.getElementById('email').value = ""
+    e.preventDefault()
 
 })
